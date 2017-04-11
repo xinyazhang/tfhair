4274a541a096cf56591a1ff7b4be8ad103fd7091
Xinya Zhang <xinyazhang@utexas.edu>
2017-04-11 13:08:04 -0500

    mask out python3 wheels.
    We focus on python2 for now.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,11 @@ unamestr=`uname`
 wheelurl=''
 
 if [[ "$unamestr" == 'Darwin' ]]; then
-	wheelurl='https://drive.google.com/open?id=0B9HTCbpQs6J1aHJ6a3gyRkhzTzA'
+	#wheelurl='https://drive.google.com/open?id=0B9HTCbpQs6J1aHJ6a3gyRkhzTzA'
 elif [[ "$unamestr" == "Linux" ]] ; then
 	hostdomain=`hostname -d`
 	if [[ "$hostdomain" == 'cs.utexas.edu' ]]; then
-		wheelurl='https://storage.googleapis.com/tensorflow-community-wheels/feb28.tf1cpu.tensorflow-1.0.0-cp35-cp35m-linux_x86_64.whl'
+		#wheelurl='https://storage.googleapis.com/tensorflow_sourced/tensorflow-1.0.1-cp35-cp35m-linux_x86_64.whl'
 	fi
 fi
 
@@ -25,7 +25,7 @@ echo "virtualenv created"
 source $venvdir/bin/activate
 echo "virtualenv activated"
 
-pip3 install --upgrade $wheelurl
+pip install --upgrade $wheelurl
 
 echo "TensorFlow installed under virtualenv directory $venvdir"
 echo "Leaving"

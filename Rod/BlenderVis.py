ceab9ec3da5281a7b48dff2bfbaf4c82301d57e9
tycheng <skysource.tony@gmail.com>
2017-04-12 14:33:36 -0500

    BlenderVis: add a very primitive test for spline visualilzation

--- Rod/BlenderVis.py
+++ Rod/BlenderVis.py
@@ -18,6 +18,12 @@ import mathutils
 
 scene = bpy.context.scene
 
+def delete_object_by_name(name):
+    if name in bpy.data.objects:
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.data.objects[name].select = True
+        bpy.ops.object.delete()
+
 class RodState(object):
 
     def __init__(self, name, xs, thetas):
@@ -31,26 +37,23 @@ class RodState(object):
         curve_data = bpy.data.curves.new("%s.centerline" % self.name, type='CURVE')
         polyline = curve_data.splines.new("NURBS")
         polyline.points.add(len(self.xs))
-        for i, (x, y, z) in enumerate(xs):
+        for i, (x, y, z) in enumerate(self.xs):
             polyline.points[i].co = (x, y, z, 1.0)
         self.centerline = bpy.data.objects.new("%s.centerline" % self.name, curve_data)
-        # add to scene
         scene.objects.link(self.centerline)
+
+    def select(self):
         scene.objects.active = self.centerline
         self.centerline.select = True
 
+def init_scene():
+    delete_object_by_name("Cube")
 
-for ob in bpy.data.objects:
-    print (ob.name)
-    try:
-        ob.material_slot_remove()
-        print ("removed material from " + ob.name)
-    except:
-        print (ob.name + " does not have materials.")
-
-if __name__ == "__main__":
-    print(scene)
-    print(bpy.data.objects)
-    xs = [ [-1.0, 1.0, 0.0], [ 0.0, 0.0, 0.0], [ 1.0, 1.0, 0.0] ]
+def init_rods():
+    xs = [ [-1.0, 1.0, 0.0], [ 0.0, 0.0, 0.0], [ 1.0, 1.0, 0.0], [ 2.0, 0.0, 0.0] ]
     thetas = [ 0.0, 0.0, 0.0 ]
     rod = RodState("rod", xs, thetas)
+
+if __name__ == "__main__":
+    init_scene()
+    init_rods()

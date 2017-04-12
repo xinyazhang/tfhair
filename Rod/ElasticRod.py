ElasticRod: fix the gradients.

Rod/ElasticRod.py
@@ -20,9 +20,11 @@ class ElasticRod:
 
     xs = None
     thetas = None
+    restl = None
 
-    def __init__(self, segments, thetas):
-        self.xs = segments
+    def __init__(self, verts, restlength, thetas):
+        self.xs = verts
+        self.restl = restlength
         self.thetas = thetas
 
     '''
@@ -70,7 +72,9 @@ def TFGetCurvature(ev, enorms):
 
 def TFGetEBend(rod):
     rod.evec = TFGetEdgeVector(rod.xs)
-    rod.enorms = TFGetEdgeLength(rod.evec)
-    rod.ls = TFGetVoronoiEdgeLength(rod.enorms)
-    rod.ks = TFGetCurvature(rod.evec, rod.enorms)
-    return tf.reduce_sum(tf.multiply(tf.norm(rod.ks, ord=2, axis=1), 1.0/rod.ls))
+#    rod.enorms = TFGetEdgeLength(rod.evec)
+    rod.restvl = TFGetVoronoiEdgeLength(rod.restl)
+    rod.ks = TFGetCurvature(rod.evec, rod.restl)
+    #return tf.reduce_sum(tf.multiply(tf.norm(rod.ks, ord=2, axis=1), 1.0/rod.restvl))
+    sqnorm = tf.reduce_sum(tf.multiply(rod.ks, rod.ks), 1, keep_dims=False)
+    return tf.reduce_sum(tf.multiply(sqnorm, 1.0/rod.restvl))

Rod/test.py
@@ -3,30 +3,40 @@
 import tensorflow as tf
 import numpy as np
 from ElasticRod import *
+import RodHelper as helper
 
 sess = tf.InteractiveSession()
+
+testi=tf.placeholder(tf.float32, shape=[1])
+testo=tf.gradients(-testi*testi, testi)[0]
+
+print(testo.eval(feed_dict={testi:[0]}))
 
 n = 3
 
 # Input placeholder
-irod = ElasticRod(tf.placeholder(tf.float32, shape=[n+1, 3]), tf.placeholder(tf.float32, shape=[n]))
+irod = ElasticRod(tf.placeholder(tf.float32, shape=[n+1, 3]), tf.placeholder(tf.float32, shape=[n]), tf.placeholder(tf.float32, shape=[n]))
 
 # Output tensor
 EBend = TFGetEBend(irod)
-Force = tf.gradients(-tf.norm(EBend, ord=2), irod.xs)
+Force = tf.gradients(-EBend, irod.xs)
 
 sess.run(tf.global_variables_initializer())
 
 xs=np.array([
-    [-1,-0.1,0],
+    [-1,-1,0],
     [0,0,0],
-    [1,-0.1,0],
-    [2,0.1,0],
+    [1,-1,0],
+    [2,0,0],
     ])
+rl = helper.calculate_rest_length(xs)
+print(rl)
 thetas=np.empty(shape=[n], dtype=np.float32)
 
-print(EBend.eval(feed_dict={irod.xs:xs, irod.thetas:thetas}))
-print(Force[0].eval(feed_dict={irod.xs:xs, irod.thetas:thetas}))
+inputdict = {irod.xs:xs, irod.restl:rl, irod.thetas:thetas}
+print(irod.ks.eval(feed_dict=inputdict))
+print(EBend.eval(feed_dict=inputdict))
+print(Force[0].eval(feed_dict=inputdict))
 
 xs2=np.array([
     [-1,-0.0,0],
@@ -34,18 +44,33 @@ xs2=np.array([
     [1,-0.0,0],
     [2,0.0,0],
     ])
-print(EBend.eval(feed_dict={irod.xs:xs2, irod.thetas:thetas}))
-print(Force[0].eval(feed_dict={irod.xs:xs2, irod.thetas:thetas}))
+rl2 = helper.calculate_rest_length(xs2)
+inputdict = {irod.xs:xs2, irod.restl:rl2, irod.thetas:thetas}
+print(EBend.eval(feed_dict=inputdict))
+print(Force[0].eval(feed_dict=inputdict))
 
 xs3=np.array([
     [-1,0.0,0],
     [0,0,0],
-    [1,0.0,0],
-    [2,1e-15,0],
+    [1,1e-5,0],
+    [2,1e-5,0],
     ])
 # print("Curvatures: ", irod.ks.eval(feed_dict={irod.xs:xs3, irod.thetas:thetas}))
 # print("Voronoi Length: ", irod.ls.eval(feed_dict={irod.xs:xs3, irod.thetas:thetas}))
 # print("Energy w/o Voronoi Length: ", tf.norm(irod.ks, ord=2, axis=1).eval(feed_dict={irod.xs:xs3, irod.thetas:thetas}))
 
-print(EBend.eval(feed_dict={irod.xs:xs3, irod.thetas:thetas}))
-print(Force[0].eval(feed_dict={irod.xs:xs3, irod.thetas:thetas}))
+rl3 = helper.calculate_rest_length(xs3)
+inputdict = {irod.xs:xs3, irod.restl:rl3, irod.thetas:thetas}
+print(EBend.eval(feed_dict=inputdict))
+print(Force[0].eval(feed_dict=inputdict))
+
+xs4=np.array([
+    [-1,0.0,0],
+    [0,0,0],
+    [1,-1e-5,0],
+    [2,-1e-5,0],
+    ])
+rl4 = helper.calculate_rest_length(xs4)
+inputdict = {irod.xs:xs4, irod.restl:rl4, irod.thetas:thetas}
+print(EBend.eval(feed_dict=inputdict))
+print(Force[0].eval(feed_dict=inputdict))
0463f5c7201af3a2c999b2b9eee18677142e55a7
Xinya Zhang <xinyazhang@utexas.edu>
2017-04-20 12:41:45 -0500

    Fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,12 @@ wheelurl=''
 
 if [[ "$unamestr" == 'Darwin' ]]; then
 	#wheelurl='https://drive.google.com/open?id=0B9HTCbpQs6J1aHJ6a3gyRkhzTzA'
+	wheelurl='tensorflow'
 elif [[ "$unamestr" == "Linux" ]] ; then
 	hostdomain=`hostname -d`
 	if [[ "$hostdomain" == 'cs.utexas.edu' ]]; then
 		#wheelurl='https://storage.googleapis.com/tensorflow_sourced/tensorflow-1.0.1-cp35-cp35m-linux_x86_64.whl'
+		wheelurl='tensorflow'
 	fi
 fi
 

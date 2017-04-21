17b7ba56159e800131b6bb704e438eccb1de42af
Xinya Zhang <xinyazhang@utexas.edu>
2017-04-21 16:25:28 -0500

    test: remove Lagrangian methods.

--- Rod/RodHelper.py
+++ Rod/RodHelper.py
@@ -88,18 +88,6 @@ def create_TFRod(n):
     rod.xdots = tf.placeholder(tf.float32, shape=[n+1, 3])
     return rod
 
-def create_TFRod_variable(n, xs_init, thetas_init, restl):
-    xs_init = tf.Variable(xs_init([n+1, 3]))
-    thetas_init = tf.Variable(thetas_init([n]))
-    restl = tf.constant(restl, dtype=tf.float32)
-    return ElasticRod.ElasticRod(xs_init, restl, thetas_init)
-
-def create_TFRod_constant(n, xs_init, thetas_init, restl):
-    xs_init = tf.constant(xs_init, dtype=tf.float32)
-    thetas_init = tf.constant(thetas_init, dtype=tf.float32)
-    restl = tf.constant(restl, dtype=tf.float32)
-    return ElasticRod.ElasticRod(xs_init, restl, thetas_init)
-
 class RodSaver():
 
     def __init__(self, directory):
@@ -131,35 +119,3 @@ class RodSaver():
         filename = os.path.join(self.directory, "%d.npy" % self.frame)
         np.save(filename, results)
         self.frame += 1
-
-class Trainer():
-
-    def __init__(self, sess, opt, loss, feed_dict=None,
-            max_iter=100000, epsilon=1e-9, display=1000):
-        self.sess = sess
-        self.opt = opt
-        self.loss = loss
-        self.scalar_loss = tf.reduce_sum(loss, axis=None, keep_dims=False)
-        print('scalar_loss shape {}'.format(self.scalar_loss.get_shape()))
-        self.feed_dict = feed_dict
-        self.max_iter = max_iter
-        self.epsilon = epsilon
-        self.display = 1000
-
-    def __enter__(self):
-        self.curr_iter = self.max_iter
-        for i in xrange(self.max_iter):
-            # display loss over iterations
-            if i % self.display == 0:
-                print "--> loss at epoch %d:" % i, self.sess.run(self.loss, feed_dict=self.feed_dict)
-            # optimize loss metrics
-            self.sess.run(self.opt, feed_dict=self.feed_dict)
-            loss_value = self.sess.run(self.scalar_loss, feed_dict=self.feed_dict)
-            if math.fabs(loss_value) < self.epsilon:
-                self.curr_iter = i
-                print("--> loss at epoch {}: {}".format(i, loss_value))
-                return self
-        return self
-
-    def __exit__(self, type, value, traceback):
-        pass

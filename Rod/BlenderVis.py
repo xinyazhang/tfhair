ElasticRod: fix the calculation of reference directors.

Rod/BlenderVis.py
@@ -241,8 +241,8 @@ class RodState(object):
         material_frames = []
         for i, theta in enumerate(thetas):
             material_frames.append(self._compute_material_frame(theta, vec_xs[i], vec_xs[i+1], refd1s[i,:], refd2s[i,:]))
-        print(material_frames[0])
-        print(self._compute_euler_angle_from_frame(material_frames[i]))
+        # print(material_frames[0])
+        # print(self._compute_euler_angle_from_frame(material_frames[0]))
 
         thetas = np.pad(thetas, [0, 1], 'edge')
         # update inter control points

Rod/ElasticRod.py
@@ -80,7 +80,7 @@ def TFGetLengthConstaintFunction(rod):
 
 # For unit \alpha
 def TFParallelTransportQuaternion(prod, crod):
-    axes = tf.cross(prod.tans, crod.tans)
+    axes = -tf.cross(prod.tans, crod.tans)
     cosines = _dot(prod.tans, crod.tans)
     halfconsines = tf.sqrt((cosines + 1)/2.0)
     halfconsines = tf.stack([halfconsines], _lastdim(halfconsines)+1) # pad one dimension
@@ -89,7 +89,7 @@ def TFParallelTransportQuaternion(prod, crod):
     bcd = tf.multiply(axes, 0.5/halfconsines)
     return halfconsines, bcd
 
-def TFPropogateRefDs(prod, crod):
+def TFPropogateRefDs(prod, crod, normalize=False):
     '''
     Calculate the current reference directions from the previous time frame.
     '''
@@ -113,6 +113,9 @@ def TFPropogateRefDs(prod, crod):
     refd2s = tf.stack([prod.refd2s], axis=_lastdim(prod.refd2s)+1)
     crod.refd1s = tf.reshape(tf.matmul(R, refd1s), shape)
     crod.refd2s = tf.reshape(tf.matmul(R, refd2s), shape)
+    if normalize:
+        crod.refd1s = _normalize(crod.refd1s)
+        crod.refd2s = _normalize(crod.refd2s)
     return crod
 
 class ElasticRod:
@@ -202,7 +205,7 @@ class ElasticRod:
         nrod.InitTF()
         return nrod
 
-    def CalcPenaltyRelaxationTF(self, h, learning_rate = 1e-3):
+    def CalcPenaltyRelaxationTF(self, h, learning_rate=1e-3):
         xs = tf.Variable(np.zeros(shape=self.xs.get_shape().as_list(), dtype=np.float32), name='xs')
         relaxxdots = self.xdots + (xs - self.xs) / h
         relaxrod = ElasticRod(
@@ -222,15 +225,21 @@ class ElasticRod:
         relaxrod.loss = relaxrod.GetEConstaintTF()
         relaxrod.trainer = tf.train.AdamOptimizer(learning_rate)
         relaxrod.grads = relaxrod.trainer.compute_gradients(relaxrod.loss)
-        print('relaxrod.grads {}'.format(relaxrod.grads))
-        print('relaxrod.fullrestvl {}'.format(relaxrod.fullrestvl))
-        print('relaxrod.rho {}'.format(relaxrod.rho))
+        # print('relaxrod.grads {}'.format(relaxrod.grads))
+        # print('relaxrod.fullrestvl {}'.format(relaxrod.fullrestvl))
+        # print('relaxrod.rho {}'.format(relaxrod.rho))
         relaxrod.weighted_grads = \
             [(grad[0] / _paddim(relaxrod.fullrestvl * relaxrod.rho), grad[1]) for grad in relaxrod.grads]
         relaxrod.apply_grads_op = relaxrod.trainer.apply_gradients(relaxrod.weighted_grads)
         if (not self.refd1s is None) and (not self.refd2s is None):
-            TFPropogateRefDs(self, relaxrod)
+            TFPropogateRefDs(self, relaxrod, normalize=True)
         return relaxrod
+
+    def GetVariableList(self):
+        return [self.xs, self.xdots, self.thetas, self.omegas, self.refd1s, self.refd2s]
+
+    def UpdateVariable(self, vl):
+        [self.xs, self.xdots, self.thetas, self.omegas, self.refd1s, self.refd2s] = vl
 
     def Relax(self, sess, irod, icond):
         inputdict = helper.create_dict([irod], [icond])
@@ -250,11 +259,8 @@ class ElasticRod:
                 E = sess.run(self.loss, feed_dict=inputdict)
                 print('loss: {}'.format(E))
             '''
-        xs, xdots, thetas, omegas = sess.run([self.xs, self.xdots, self.thetas, self.omegas], feed_dict=inputdict)
-        icond.xs = xs
-        icond.xdots = xdots
-        icond.thetas = thetas
-        icond.omegas = omegas
+        vl = sess.run(self.GetVariableList(), feed_dict=inputdict)
+        icond.UpdateVariable(vl)
         return icond
 
     '''

Rod/test_M.py
@@ -26,7 +26,8 @@ def run_with_bc(n, h, rho, icond, path):
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         nframe = 720
-        with progressbar.ProgressBar(max_value=nframe-1) as progress:
+        #nframe = 10
+        with progressbar.ProgressBar(max_value=nframe) as progress:
             for frame in range(nframe):
                 #inputdict = {irod.xs:xs, irod.restl:rl, irod.thetas:thetas, irod.xdots:xdots, irod:omegas:omegas}
                 inputdict = helper.create_dict([irod], [icond])
@@ -43,7 +44,9 @@ def run_with_bc(n, h, rho, icond, path):
                 # print("xdots {}".format(xdots))
                 # print("thetas {}".format(icond.thetas))
                 icond = rrod.Relax(sess, irod, icond)
-                progress.update(frame)
+                # print("refd1s {}".format(icond.refd1s))
+                # print("refd2s {}".format(icond.refd2s))
+                progress.update(frame+1)
 
     saver.close()
 
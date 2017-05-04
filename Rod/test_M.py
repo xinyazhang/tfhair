a81aa45459540f50d694254316aff4950fc30137
tycheng <skysource.tony@gmail.com>
2017-05-04 16:41:19 -0500

    add twisting tests

--- Rod/test_M.py
+++ Rod/test_M.py
@@ -411,6 +411,93 @@ def run_test10():
 
     run_with_bc(n, h, rho, icond, '/tmp/tftest10', obstacle=obstacle)
 
+def run_test11():
+    '''
+    Test 11: twisting with anchor points
+    '''
+
+    iterator = xrange(-5,6)
+    n = len(list(iterator)) - 1
+    h = 1.0/1024.0
+    rho = 1e-2
+
+    def FixedTwister(h, icond):
+        icond.thetas[0] = 0
+        icond.thetas[-1] = pi * icond.t
+        icond.t += h * 8
+
+    xs = np.array(
+        [[i,0,0] for i in iterator]
+    )
+    xdots = np.array(
+        [[0,0,0]] * (n+1)
+    )
+    thetas = np.zeros(shape=[n], dtype=np.float32)
+    omegas = np.zeros(shape=[n], dtype=np.float32)
+    icond = helper.create_BCRod(xs=xs,
+            xdots=xdots,
+            thetas=thetas,
+            omegas=omegas,
+            initd1=np.array([0,1,0])
+            )
+    icond.sparse_anchor_indices = np.array([
+            [0],
+            [2],
+        ], dtype=np.int32)
+    icond.sparse_anchor_values = np.array([
+            xs[0,:],
+            xs[2,:],
+        ], dtype=np.float32)
+    icond.beta = 10
+    icond.t = 0
+    run_with_bc(n, h, rho, icond, '/tmp/tftest11', icond_updater=FixedTwister)
+
+def run_test12():
+    '''
+    Test 12: twisting with anchor points, version 2
+    '''
+
+    iterator1 = xrange(-1,0)
+    iterator2 = xrange(0,2)
+    n1 = len(list(iterator1))
+    n2 = len(list(iterator2))
+    n = n1 + n2 - 1
+    h = 1.0/1024.0
+    rho = 1e-2
+
+    def FixedTwisterII(h, icond):
+        icond.thetas[0] = pi * icond.t
+        icond.xs[0] = np.array([-n1, 0, 0])
+        icond.xs[n1] = np.array([0, 0, 0])
+        icond.t += h * 8
+
+    xs = np.array(
+        [[i,0,0] for i in iterator1] +
+        [[0,i,0] for i in iterator2]
+    )
+    xdots = np.array(
+        [[0,0,0]] * (n+1)
+    )
+    thetas = np.zeros(shape=[n], dtype=np.float32)
+    omegas = np.zeros(shape=[n], dtype=np.float32)
+    icond = helper.create_BCRod(xs=xs,
+            xdots=xdots,
+            thetas=thetas,
+            omegas=omegas,
+            initd1=np.array([0,1,0])
+            )
+    icond.sparse_anchor_indices = np.array([
+            [0],
+            [2],
+        ], dtype=np.int32)
+    icond.sparse_anchor_values = np.array([
+            xs[0,:],
+            xs[2,:],
+        ], dtype=np.float32)
+    icond.beta = 10
+    icond.t = 0
+    run_with_bc(n, h, rho, icond, '/tmp/tftest12', icond_updater=FixedTwisterII)
+
 def run():
     run_test0()
     run_test1()
@@ -423,6 +510,8 @@ def run():
     run_test8()
     run_test9()
     run_test10()
+    run_test11()
+    run_test12()
 
 if __name__ == '__main__':
     run()
